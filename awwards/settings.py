70895e579d6826cee01ae80d255709f0408dcfc1
rachel <rachelkemuma99@gmail.com>
2022-06-11 14:10:02 +0300

    create database and make changes to the database in settings

--- awwards/settings.py
+++ awwards/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'awwards.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'awwards',
+        'USER': 'rachel',
+        'PASSWORD': 'hotspurs'
     }
 }
 
