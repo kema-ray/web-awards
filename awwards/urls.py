5615acc90b0f8de3c3f4d6a46aa43da9a4d893ce
rachel <rachelkemuma99@gmail.com>
2022-06-11 15:01:39 +0300

    create the register account page

--- awwards/urls.py
+++ awwards/urls.py
@@ -20,4 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('myawwards.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
